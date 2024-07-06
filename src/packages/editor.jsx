63d86f77bf99c2c93731fb4d6d8c60d410c9d2b4
hejuxin <cuobingfeicuo@126.com>
2024-07-07 02:24:17 +0800

    feat: 封装拖拽相关逻辑

--- src/packages/editor.jsx
+++ src/packages/editor.jsx
@@ -1,6 +1,7 @@
 import { computed, defineComponent, inject, ref } from "vue";
 import EditorBlock from "./editor-block";
 import './editor.scss';
+import { useMenuDragger } from "@/utils/useMenuDragger";
 
 export default defineComponent({
   props: ["modelValue"],
@@ -23,53 +24,7 @@ export default defineComponent({
     const config = inject('config');
 
     const contentRef = ref(null);
-    let currentComponent = null;
-
-    const dragenter = (e) => {
-      e.dataTransfer.dropEffect = 'move';
-    }
-
-    const dragover = (e) => {
-      e.preventDefault();
-    }
-
-    const dragleave = (e) => {
-      e.dataTransfer.dropEffect = 'none';
-    }
-
-    const drop = e => {
-      const blocks = data.value.blocks;
-      const newData = {
-        ...data.value,
-        blocks: [
-          ...blocks,
-          {
-            top: e.offsetY,
-            left: e.offsetX,
-            zIndex: 1,
-            key: currentComponent.key
-          }
-        ]
-      }
-
-      data.value = newData;
-      currentComponent = null;
-    }
-
-    const handleDragStart = (e, component) => {
-      currentComponent = component;
-      contentRef.value.addEventListener('dragenter', dragenter);
-      contentRef.value.addEventListener('dragover', dragover);
-      contentRef.value.addEventListener('dragleave', dragleave);
-      contentRef.value.addEventListener('drop', drop);
-    }
-
-    const handleDragEnd = (e) => {
-      contentRef.value.removeEventListener('dragenter', dragenter);
-      contentRef.value.removeEventListener('dragover', dragover);
-      contentRef.value.removeEventListener('dragleave', dragleave);
-      contentRef.value.removeEventListener('drop', drop);
-    }
+    const { handleDragStart, handleDragEnd } = useMenuDragger(contentRef, data);
 
     return () => (
       <div className="editor">
---
+++ src/utils/useMenuDragger.js
@@ -0,0 +1,54 @@
+export function useMenuDragger(contentRef, data) {
+  let currentComponent = null;
+
+  const dragenter = (e) => {
+    e.dataTransfer.dropEffect = 'move';
+  }
+
+  const dragover = (e) => {
+    e.preventDefault();
+  }
+
+  const dragleave = (e) => {
+    e.dataTransfer.dropEffect = 'none';
+  }
+
+  const drop = e => {
+    const blocks = data.value.blocks;
+    const newData = {
+      ...data.value,
+      blocks: [
+        ...blocks,
+        {
+          top: e.offsetY,
+          left: e.offsetX,
+          zIndex: 1,
+          key: currentComponent.key
+        }
+      ]
+    }
+
+    data.value = newData;
+    currentComponent = null;
+  }
+
+  const handleDragStart = (e, component) => {
+    currentComponent = component;
+    contentRef.value.addEventListener('dragenter', dragenter);
+    contentRef.value.addEventListener('dragover', dragover);
+    contentRef.value.addEventListener('dragleave', dragleave);
+    contentRef.value.addEventListener('drop', drop);
+  }
+
+  const handleDragEnd = (e) => {
+    contentRef.value.removeEventListener('dragenter', dragenter);
+    contentRef.value.removeEventListener('dragover', dragover);
+    contentRef.value.removeEventListener('dragleave', dragleave);
+    contentRef.value.removeEventListener('drop', drop);
+  }
+
+  return {
+    handleDragStart,
+    handleDragEnd
+  }
+}
