3bfda0b506832c1402aed448208a26e6e32b1a19
hejuxin <cuobingfeicuo@126.com>
2024-07-07 02:33:28 +0800

    feat: 添加相关注释

--- src/utils/useMenuDragger.js
+++ src/utils/useMenuDragger.js
@@ -1,18 +1,22 @@
 export function useMenuDragger(contentRef, data) {
   let currentComponent = null;
 
+  // dragenter进入元素中 添加一个移动的标识
   const dragenter = (e) => {
     e.dataTransfer.dropEffect = 'move';
   }
 
+  // dragover 在目标元素经过 必须要阻止默认行为 否则不能触发drop
   const dragover = (e) => {
     e.preventDefault();
   }
 
+  // dragleave 离开元素的时候 需要增加一个禁用标识
   const dragleave = (e) => {
     e.dataTransfer.dropEffect = 'none';
   }
 
+  // drop 松手的时候 根据拖拽的组件 添加一个组件
   const drop = e => {
     const blocks = data.value.blocks;
     const newData = {
